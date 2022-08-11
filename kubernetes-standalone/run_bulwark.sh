fcd1feb83bc3d7b88a37aa13411353794f45e226
Leo Kosta <kostaleonard@gmail.com>
2022-08-11 18:49:18 -0400

    Added wireguard container
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

---
+++ kubernetes-standalone/run_bulwark.sh
@@ -0,0 +1,27 @@
+# TODO remove this file.
+# For testing.
+
+docker run -d \
+  --rm \
+  --name=bulwark \
+  --cap-add=NET_ADMIN \
+  --cap-add=SYS_MODULE \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Europe/London \
+  -e PEERS="leo_mac" \
+  -e SERVERURL=wireguard.domain.com \
+  -e SERVERPORT=51820 \
+  -e INTERNAL_SUBNET=10.13.13.0 \
+  -e PEERDNS=auto \
+  -p 51820:51820/udp \
+  -v /tmp/bulwark-config:/config \
+  -v /lib/modules:/lib/modules \
+  --sysctl="net.ipv4.conf.all.src_valid_mark=1" \
+  linuxserver/wireguard
+
+
+
+    #-e ALLOWEDIPS=0.0.0.0/0 `#optional` \
+    #-e LOG_CONFS=true `#optional` \
+    #--restart unless-stopped \
